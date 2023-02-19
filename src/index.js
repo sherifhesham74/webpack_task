8f1b1b43304009ae887427e368d212c2193e64b5
sherifhesham74 <100443942+sherifhesham74@users.noreply.github.com>
2023-02-19 22:56:47 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import hello from "./hello world"
+hello()
+
+
+
+
+
+import "./style.css"
+
+
+
+import "./component/header/header"
+import"./component/cover/cover"
+
+import"./component/bio/bio"
+
+
+import"./assets/city.png"
+
+
+
+
+
+
+// // //
+// // /* 
+// // this is a big comment
+
+
+
+
+
+
+
+
+
+// //  */
